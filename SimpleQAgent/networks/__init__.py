5380f659550a5f4f0befa511905738fc76157fdc
SergioMtz99 <asimplethings.ast@gmail.com>
2024-06-21 16:03:18 -0600

    added dueling networks

--- SimpleQAgent/networks/__init__.py
+++ SimpleQAgent/networks/__init__.py
@@ -1,2 +1,2 @@
-from SimpleQAgent.networks.deep_q_network import *
-from SimpleQAgent.networks.networks import *
+from SimpleQAgent.networks.deep_q_network import DeepQNetwork
+from SimpleQAgent.networks.networks import ConvNetwork, LinearNetwork, DuelingLinearNetwork, DuelingConvNetwork
